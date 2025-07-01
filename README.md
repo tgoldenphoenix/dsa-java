5b911de4109302f7189626a5b5f2fd633596f1e5
tgoldenphoenix <anhaophamx@gmail.com>
2025-07-01 09:47:37 +0700

    add varargs

---
+++ README.md
@@ -0,0 +1,3 @@
+# DSA Java
+
+learn Java code
---
+++ src/main/java/com/anhao/syntax/Vararg.java
@@ -0,0 +1,31 @@
+package com.anhao.syntax;
+
+/*
+Varargs = variable arguments
+- Avoid overloading many times
+
+No need to pass an already initialized array as argument, just pass them normally
+```
+int[] params = {1, 2, 3, 4, 5};
+sum(params); // 15
+```
+*/
+
+public class Vararg {
+    public static void main(String[] args) {
+        System.out.println("Sum: " + sum(1, 2));
+    }
+
+    public static int sum(int... args) {
+        int result = 0;
+        for (int i=0; i<args.length; i++)
+            result += args[i];
+        return result;
+    }
+}
+
+/*
+Notes:
+- Varargs must be the last argument
+- One method can only have one varargs
+*/
