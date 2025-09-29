221c64e665570dc1aa9dda0e44a6e1bf93178c9e
tgoldenphoenix <anhaophamx@gmail.com>
2025-09-29 09:08:11 +0700

    learn more DSA

--- src/main/java/com/anhao/algorithms/LeetCodeDebug.java
+++ src/main/java/com/anhao/algorithms/LeetCodeDebug.java
@@ -1,6 +1,9 @@
 package com.anhao.algorithms;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
 
 class Solution {
   public static void merge(int[] nums1, int m, int[] nums2, int n) {
@@ -36,5 +39,63 @@ public class LeetCodeDebug {
 
     Solution.merge(nums1, 3, nums2, 3);
     System.out.println(Arrays.toString(nums1));
+  }
+}
+
+class LRUCache {
+  // capacity of the cache
+  private int capacity;
+  private Map<Integer, LinkedListNode> cache;
+  // LinkedList stores the order of the cached item
+  // front is most recent, tail is least recent
+  private LinkedList<LinkedListNode> lruList;
+
+  class LinkedListNode {
+    // key-value pair
+    int key;
+    int value;
+
+    // constructor
+    LinkedListNode(int key, int value) {
+      this.key = key;
+      this.value = value;
+    }
+  }
+  // constructor
+  public LRUCache(int capacity) {
+    this.capacity = capacity;
+    cache = new HashMap<>();
+    lruList = new LinkedList<>();
+  }
+
+  public int get(int key) {
+    if (cache.containsKey(key)) {
+      LinkedListNode node = cache.get(key);
+      lruList.remove(node);
+      // add to the front
+      lruList.addFirst(node);
+      return node.value;
+    }
+    return -1;
+  }
+  public void put(int key, int value) {
+    if (cache.containsKey(key)) {
+      // update an existing cache item's value
+      LinkedListNode node = cache.get(key);
+      lruList.remove(node);
+      // update new value for the node
+      node.value = value;
+      lruList.addFirst(node);
+    } else {
+      // add a new value to the cache
+      if (cache.size() >= capacity) {
+        // If the cache already at its max capacity, we must delete the least recently use item
+        LinkedListNode node = lruList.removeLast();
+        cache.remove(node.key);
+      }
+      LinkedListNode node = new LinkedListNode(key, value);
+      lruList.addFirst(node);
+      cache.put(key, node);
+    }
   }
 }
