90314e1e0b8d65f9f8a783042624a1703819c71b
tgoldenphoenix <anhaophamx@gmail.com>
2025-10-02 19:37:23 +0700

    learn more java

--- src/main/java/com/anhao/arrays/SwapNReverseArray.java
+++ src/main/java/com/anhao/arrays/SwapNReverseArray.java
@@ -2,7 +2,7 @@ package com.anhao.arrays;
 
 import java.util.Arrays;
 
-public class Swap {
+public class SwapNReverseArray {
     public static void main(String[] args) {
         int[] arr = {1, 3, 23, 9, 18, 56};
 //        swap(arr, 0, 4);
